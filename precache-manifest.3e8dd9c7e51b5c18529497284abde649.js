18a5898a76b215e746c199fcb8142b956a252e0f
arianna choza <unachoza@gmail.com>
2020-04-22 10:31:21 -0400

    Updates

---
+++ precache-manifest.3e8dd9c7e51b5c18529497284abde649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024b08eeebf0b51bc4119413f607c927",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e231d65b33b006dd4515",
+    "url": "/react-gh-pages/static/css/main.041f3b74.chunk.css"
+  },
+  {
+    "revision": "2e32c0edf289d900ca51",
+    "url": "/react-gh-pages/static/js/2.875e5e6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.875e5e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e231d65b33b006dd4515",
+    "url": "/react-gh-pages/static/js/main.55c9ff21.chunk.js"
+  },
+  {
+    "revision": "603e67b5e057e644ad95",
+    "url": "/react-gh-pages/static/js/runtime-main.df42c444.js"
+  }
+]);
